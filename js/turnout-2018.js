d39cc0fe11b2c81cfb4658156bdfdfe67a4725cc
Sarah H. Whitmore <sarahwhitmore15@gmail.com>
2019-03-21 15:18:43 -0400

    initial commit

---
+++ js/turnout-2018.js
@@ -0,0 +1,461 @@
+var turnout = [
+ {
+   "state": "Alabama",
+   "abbrev": "AL",
+   "turnout-2018-pct": 100,
+   "turnout": 1350000,
+   "vep": 3641209,
+   "vap": 3802714,
+   "non-citizen-pct": 2.5
+ },
+ {
+   "state": "Alaska",
+   "abbrev": "AK",
+   "turnout-2018-pct": 0,
+   "turnout": 300000,
+   "vep": 521777,
+   "vap": 554426,
+   "non-citizen-pct": 4.1
+ },
+ {
+   "state": "Arizona",
+   "abbrev": "AZ",
+   "turnout-2018-pct": 0,
+   "turnout": 2200000,
+   "vep": 4910625,
+   "vap": 5519036,
+   "non-citizen-pct": 9.4
+ },
+ {
+   "state": "Arkansas",
+   "abbrev": "AR",
+   "turnout-2018-pct": 100,
+   "turnout": 900000,
+   "vep": 2171940,
+   "vap": 2319740,
+   "non-citizen-pct": 3.8
+ },
+ {
+   "state": "California",
+   "abbrev": "CA",
+   "turnout-2018-pct": 0,
+   "turnout": 11000000,
+   "vep": 25635139,
+   "vap": 30836229,
+   "non-citizen-pct": 16.4
+ },
+ {
+   "state": "Colorado",
+   "abbrev": "CO",
+   "turnout-2018-pct": 0,
+   "turnout": 2250000,
+   "vep": 4103903,
+   "vap": 4445013,
+   "non-citizen-pct": 7
+ },
+ {
+   "state": "Connecticut",
+   "abbrev": "CT",
+   "turnout-2018-pct": 0,
+   "turnout": 1200000,
+   "vep": 2614176,
+   "vap": 2856023,
+   "non-citizen-pct": 7.8
+ },
+ {
+   "state": "Delaware",
+   "abbrev": "DE",
+   "turnout-2018-pct": 0,
+   "turnout": 275000,
+   "vep": 710434,
+   "vap": 769716,
+   "non-citizen-pct": 5.7
+ },
+ {
+   "state": "District of Columbia",
+   "abbrev": "DC",
+   "turnout-2018-pct": 100,
+   "turnout": 200000,
+   "vep": 529198,
+   "vap": 577553,
+   "non-citizen-pct": 8.4
+ },
+ {
+   "state": "Florida",
+   "abbrev": "FL",
+   "turnout-2018-pct": 100,
+   "turnout": 8000000,
+   "vep": 15140654,
+   "vap": 17168712,
+   "non-citizen-pct": 10.5
+ },
+ {
+   "state": "Georgia",
+   "abbrev": "GA",
+   "turnout-2018-pct": 100,
+   "turnout": 3500000,
+   "vep": 7178075,
+   "vap": 8063141,
+   "non-citizen-pct": 7.2
+ },
+ {
+   "state": "Hawaii",
+   "abbrev": "HI",
+   "turnout-2018-pct": 0,
+   "turnout": 400000,
+   "vep": 1014363,
+   "vap": 1122119,
+   "non-citizen-pct": 9.1
+ },
+ {
+   "state": "Idaho",
+   "abbrev": "ID",
+   "turnout-2018-pct": 0,
+   "turnout": 500000,
+   "vep": 1224275,
+   "vap": 1314172,
+   "non-citizen-pct": 4.4
+ },
+ {
+   "state": "Illinois",
+   "abbrev": "IL",
+   "turnout-2018-pct": 100,
+   "turnout": 4100000,
+   "vep": 9017850,
+   "vap": 9902219,
+   "non-citizen-pct": 8.5
+ },
+ {
+   "state": "Indiana",
+   "abbrev": "IN",
+   "turnout-2018-pct": 100,
+   "turnout": 2200000,
+   "vep": 4923068,
+   "vap": 5139352,
+   "non-citizen-pct": 3.7
+ },
+ {
+   "state": "Iowa",
+   "abbrev": "IA",
+   "turnout-2018-pct": 0,
+   "turnout": 1300000,
+   "vep": 2312204,
+   "vap": 2431468,
+   "non-citizen-pct": 3.6
+ },
+ {
+   "state": "Kansas",
+   "abbrev": "KS",
+   "turnout-2018-pct": 0,
+   "turnout": 1000000,
+   "vep": 2072292,
+   "vap": 2211696,
+   "non-citizen-pct": 5.2
+ },
+ {
+   "state": "Kentucky",
+   "abbrev": "KY",
+   "turnout-2018-pct": 100,
+   "turnout": 1200000,
+   "vep": 3319907,
+   "vap": 3469061,
+   "non-citizen-pct": 2.4
+ },
+ {
+   "state": "Louisiana",
+   "abbrev": "LA",
+   "turnout-2018-pct": 0,
+   "turnout": 1300000,
+   "vep": 3394257,
+   "vap": 3581751,
+   "non-citizen-pct": 2.7
+ },
+ {
+   "state": "Maine",
+   "abbrev": "ME",
+   "turnout-2018-pct": 0,
+   "turnout": 650000,
+   "vep": 1073006,
+   "vap": 1092942,
+   "non-citizen-pct": 1.8
+ },
+ {
+   "state": "Maryland",
+   "abbrev": "MD",
+   "turnout-2018-pct": 100,
+   "turnout": 2000000,
+   "vep": 4306482,
+   "vap": 4742399,
+   "non-citizen-pct": 8.8
+ },
+ {
+   "state": "Massachusetts",
+   "abbrev": "MA",
+   "turnout-2018-pct": 0,
+   "turnout": 2500000,
+   "vep": 5042403,
+   "vap": 5546080,
+   "non-citizen-pct": 8.9
+ },
+ {
+   "state": "Michigan",
+   "abbrev": "MI",
+   "turnout-2018-pct": 100,
+   "turnout": 4000000,
+   "vep": 7513813,
+   "vap": 7841291,
+   "non-citizen-pct": 3.7
+ },
+ {
+   "state": "Minnesota",
+   "abbrev": "MN",
+   "turnout-2018-pct": 0,
+   "turnout": 2500000,
+   "vep": 4064389,
+   "vap": 4335998,
+   "non-citizen-pct": 4.6
+ },
+ {
+   "state": "Mississippi",
+   "abbrev": "MS",
+   "turnout-2018-pct": 100,
+   "turnout": 850000,
+   "vep": 2201276,
+   "vap": 2278270,
+   "non-citizen-pct": 1.4
+ },
+ {
+   "state": "Missouri",
+   "abbrev": "MO",
+   "turnout-2018-pct": 0,
+   "turnout": 2250000,
+   "vep": 4570675,
+   "vap": 4764892,
+   "non-citizen-pct": 2.5
+ },
+ {
+   "state": "Montana",
+   "abbrev": "MT",
+   "turnout-2018-pct": 0,
+   "turnout": 465000,
+   "vep": 821710,
+   "vap": 835371,
+   "non-citizen-pct": 1.2
+ },
+ {
+   "state": "Nebraska",
+   "abbrev": "NE",
+   "turnout-2018-pct": 0,
+   "turnout": 600000,
+   "vep": 1364285,
+   "vap": 1458088,
+   "non-citizen-pct": 5.5
+ },
+ {
+   "state": "Nevada",
+   "abbrev": "NV",
+   "turnout-2018-pct": 0,
+   "turnout": 930000,
+   "vep": 2052636,
+   "vap": 2380512,
+   "non-citizen-pct": 12.6
+ },
+ {
+   "state": "New Hampshire",
+   "abbrev": "NH",
+   "turnout-2018-pct": 0,
+   "turnout": 550000,
+   "vep": 1062391,
+   "vap": 1096918,
+   "non-citizen-pct": 2.9
+ },
+ {
+   "state": "New Jersey",
+   "abbrev": "NJ",
+   "turnout-2018-pct": 100,
+   "turnout": 2700000,
+   "vep": 6114344,
+   "vap": 7074259,
+   "non-citizen-pct": 12
+ },
+ {
+   "state": "New Mexico",
+   "abbrev": "NM",
+   "turnout-2018-pct": 0,
+   "turnout": 675000,
+   "vep": 1482246,
+   "vap": 1610112,
+   "non-citizen-pct": 6.9
+ },
+ {
+   "state": "New York",
+   "abbrev": "NY",
+   "turnout-2018-pct": 100,
+   "turnout": 5500000,
+   "vep": 13772077,
+   "vap": 15748183,
+   "non-citizen-pct": 12
+ },
+ {
+   "state": "North Carolina",
+   "abbrev": "NC",
+   "turnout-2018-pct": 100,
+   "turnout": 3200000,
+   "vep": 7577011,
+   "vap": 8117590,
+   "non-citizen-pct": 5.5
+ },
+ {
+   "state": "North Dakota",
+   "abbrev": "ND",
+   "turnout-2018-pct": 0,
+   "turnout": 300000,
+   "vep": 564114,
+   "vap": 578460,
+   "non-citizen-pct": 2.2
+ },
+ {
+   "state": "Ohio",
+   "abbrev": "OH",
+   "turnout-2018-pct": 100,
+   "turnout": 4250000,
+   "vep": 8839520,
+   "vap": 9113230,
+   "non-citizen-pct": 2.4
+ },
+ {
+   "state": "Oklahoma",
+   "abbrev": "OK",
+   "turnout-2018-pct": 0,
+   "turnout": 1150000,
+   "vep": 2798385,
+   "vap": 2987070,
+   "non-citizen-pct": 4.7
+ },
+ {
+   "state": "Oregon",
+   "abbrev": "OR",
+   "turnout-2018-pct": 0,
+   "turnout": 1600000,
+   "vep": 3113178,
+   "vap": 3339282,
+   "non-citizen-pct": 6.3
+ },
+ {
+   "state": "Pennsylvania",
+   "abbrev": "PA",
+   "turnout-2018-pct": 100,
+   "turnout": 4500000,
+   "vep": 9758386,
+   "vap": 10178261,
+   "non-citizen-pct": 3.7
+ },
+ {
+   "state": "Rhode Island",
+   "abbrev": "RI",
+   "turnout-2018-pct": 0,
+   "turnout": 375000,
+   "vep": 793460,
+   "vap": 856988,
+   "non-citizen-pct": 7.1
+ },
+ {
+   "state": "South Carolina",
+   "abbrev": "SC",
+   "turnout-2018-pct": 100,
+   "turnout": 1450000,
+   "vep": 3819264,
+   "vap": 3996952,
+   "non-citizen-pct": 3.4
+ },
+ {
+   "state": "South Dakota",
+   "abbrev": "SD",
+   "turnout-2018-pct": 0,
+   "turnout": 340000,
+   "vep": 640417,
+   "vap": 663321,
+   "non-citizen-pct": 2.5
+ },
+ {
+   "state": "Tennessee",
+   "abbrev": "TN",
+   "turnout-2018-pct": 100,
+   "turnout": 2150000,
+   "vep": 5031061,
+   "vap": 5290993,
+   "non-citizen-pct": 3.5
+ },
+ {
+   "state": "Texas",
+   "abbrev": "TX",
+   "turnout-2018-pct": 100,
+   "turnout": 7600000,
+   "vep": 18072155,
+   "vap": 21390152,
+   "non-citizen-pct": 13.3
+ },
+ {
+   "state": "Utah",
+   "abbrev": "UT",
+   "turnout-2018-pct": 0,
+   "turnout": 800000,
+   "vep": 2082549,
+   "vap": 2242122,
+   "non-citizen-pct": 6.9
+ },
+ {
+   "state": "Vermont",
+   "abbrev": "VT",
+   "turnout-2018-pct": 0,
+   "turnout": 270000,
+   "vep": 497750,
+   "vap": 508992,
+   "non-citizen-pct": 2.2
+ },
+ {
+   "state": "Virginia",
+   "abbrev": "VA",
+   "turnout-2018-pct": 100,
+   "turnout": 2800000,
+   "vep": 6141102,
+   "vap": 6673763,
+   "non-citizen-pct": 6.9
+ },
+ {
+   "state": "Washington",
+   "abbrev": "WA",
+   "turnout-2018-pct": 100,
+   "turnout": 2600000,
+   "vep": 5316196,
+   "vap": 5903135,
+   "non-citizen-pct": 8.6
+ },
+ {
+   "state": "West Virginia",
+   "abbrev": "WV",
+   "turnout-2018-pct": 100,
+   "turnout": 600000,
+   "vep": 1405262,
+   "vap": 1435238,
+   "non-citizen-pct": 1.1
+ },
+ {
+   "state": "Wisconsin",
+   "abbrev": "WI",
+   "turnout-2018-pct": 0,
+   "turnout": 2600000,
+   "vep": 4337051,
+   "vap": 4549459,
+   "non-citizen-pct": 3.2
+ },
+ {
+   "state": "Wyoming",
+   "abbrev": "WY",
+   "turnout-2018-pct": 0,
+   "turnout": 200000,
+   "vep": 421722,
+   "vap": 438237,
+   "non-citizen-pct": 2.4
+ }
+];
